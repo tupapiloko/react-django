688cc9a2806b08c513c1b955b03ac7fb3b837e35
TuPapiLoko <baquedavid0@gmail.com>
2022-02-02 17:09:39 +0100

    vuamos

--- users/api/router.py
+++ users/api/router.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
 from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
@@ -9,5 +10,6 @@ router_user.register(
 )
 
 urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obatain_pair'),
     path('auth/me/', UserView.as_view())
 ]
