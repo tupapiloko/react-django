c3cf5a921815eda53284b97ad8f9979aa146dcc3
TuPapiLoko <baquedavid0@gmail.com>
2022-02-02 15:14:04 +0100

    vnsq

--- users/api/views.py
+++ users/api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
+
 from users.models import User
 from users.api.serializers import UserSerializer
 
+
 class UserApiViewSet(ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
@@ -20,3 +24,11 @@ class UserApiViewSet(ModelViewSet):
         else:
             request.data['password'] = request.user.password
         return super().update(request, *args, **kwargs)
+
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
